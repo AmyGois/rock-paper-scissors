818a224f54eb36e4d622a07295d656bc5f962d49
AmyGois <ahagois@hotmail.com>
2022-08-25 20:06:35 +0100

    Create function for 1 round of game
    Return victory status at the end of the round.

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function getComputerChoice() {
     }
 }
 
-/* Get player's choice: rock, paper or scissors. Disallow other options */
+/* Get player's choice: rock, paper or scissors. Disallow other inputs */
 function getPlayerChoice() {
     let choice = prompt('Pick: rock, paper or scissors', '').toLowerCase();
     
@@ -26,3 +26,22 @@ function getPlayerChoice() {
     }
 }
 
+/* Play 1 round of rock, paper, scissors. Return victory, loss or tie */
+const computerSelection = getComputerChoice();
+const playerSelection = getPlayerChoice();
+
+function playRound() {
+    
+    if((playerSelection === 'rock' && computerSelection === 'scissors') | (playerSelection === 'scissors' && computerSelection === 'paper') | (playerSelection === 'paper' && computerSelection === 'rock')) {
+        const winMessage = `You win this round! ${playerSelection} beats ${computerSelection}.`;
+        return winMessage;
+    } else if((playerSelection === 'rock' && computerSelection === 'paper') | (playerSelection === 'scissors' && computerSelection === 'rock') | (playerSelection === 'paper' && computerSelection === 'scissors')) {
+        const loseMessage = `You lose this round! ${computerSelection} beats ${playerSelection}.`;
+        return loseMessage;
+    } else if (playerSelection === computerSelection) {
+        const tieMessage = `It's a tie this round! ${playerSelection} and ${computerSelection}.`;
+        return tieMessage;
+    }
+}
+
+console.log(playRound());
