4a673901a170720bd32ecebc5f3ebe534b980cb3
AmyGois <ahagois@hotmail.com>
2022-08-26 13:52:40 +0100

    Create function for 5 round game, edit playRound
    Return victory status at the end of 5 rounds. Edit playRound function
    to store wins, losses and ties in variables.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 
-/* Generate random choice: rock, paper or scissors */
+/* F1 - Generate random choice: rock, paper or scissors */
+
 function getComputerChoice() {
     const randomNumber = Math.floor(Math.random() * (4-1) + 1);
 
@@ -12,7 +13,8 @@ function getComputerChoice() {
     }
 }
 
-/* Get player's choice: rock, paper or scissors. Disallow other inputs */
+/* F2 - Get player's choice: rock, paper or scissors. Disallow other inputs */
+
 function getPlayerChoice() {
     let choice = prompt('Pick: rock, paper or scissors', '').toLowerCase();
     
@@ -26,22 +28,64 @@ function getPlayerChoice() {
     }
 }
 
-/* Play 1 round of rock, paper, scissors. Return victory, loss or tie */
-const computerSelection = getComputerChoice();
-const playerSelection = getPlayerChoice();
+/* F3 - Play 1 round of rock, paper, scissors. Return victory, loss or tie
+    Requires F1 and F2 */
+let wins = 0;
+let losses = 0;
+let ties = 0;
 
 function playRound() {
-    
+    const computerSelection = getComputerChoice();
+    const playerSelection = getPlayerChoice();
+
     if((playerSelection === 'rock' && computerSelection === 'scissors') | (playerSelection === 'scissors' && computerSelection === 'paper') | (playerSelection === 'paper' && computerSelection === 'rock')) {
         const winMessage = `You win this round! ${playerSelection} beats ${computerSelection}.`;
+        wins += 1;
         return winMessage;
     } else if((playerSelection === 'rock' && computerSelection === 'paper') | (playerSelection === 'scissors' && computerSelection === 'rock') | (playerSelection === 'paper' && computerSelection === 'scissors')) {
         const loseMessage = `You lose this round! ${computerSelection} beats ${playerSelection}.`;
+        losses += 1;
         return loseMessage;
     } else if (playerSelection === computerSelection) {
         const tieMessage = `It's a tie this round! ${playerSelection} and ${computerSelection}.`;
+        ties += 1;
         return tieMessage;
+    } else {
+        return 'Oops! Something went wrong. Sorry.'
     }
 }
 
-console.log(playRound());
+/* F4 - Play a 5 round game. Return final victory, loss or tie
+    Requires F3 */
+
+function game() {
+    let result;
+
+    for(let i = 0; i < 5; i++) {
+
+        console.log(playRound());
+
+    }
+
+    if(wins > losses) {
+        return result = `You Won! Yay!
+        Games won: ${wins}
+        Games lost: ${losses}
+        Games tied: ${ties}`;
+        
+    } else if(wins < losses) {
+        return result = `You Lost! Maybe next time...
+        Games won: ${wins}
+        Games lost: ${losses}
+        Games tied: ${ties}`;
+    } else if(wins === losses) {
+        return result = `You tried, you tied!
+        Games won: ${wins}
+        Games lost: ${losses}
+        Games tied: ${ties}`;
+    } else {
+        return 'Oops! Something went wrong. Sorry.'
+    }
+}
+
+console.log(game());
