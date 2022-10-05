b4bcf262f5d42bd794eff7d4214f2152a20da0e3
AmyGois <ahagois@hotmail.com>
2022-10-05 18:44:08 +0100

    End and reset the game; organise script.js
    Create gmae end and reset UI in index.html. Create game end and reset
    functions in script.js. Organise script.js to be easier to read.

--- script.js
+++ script.js
@@ -9,8 +9,68 @@ const computerChoiceName = document.querySelector("#computerChoiceName");
 
 const roundResult = document.querySelector("#roundResult");
 
+let wins = 0;
+let losses = 0;
+let ties = 0;
 
-/* F1 - Generate random choice: rock, paper or scissors */
+const playerWinsText = document.querySelector("#playerWins");
+const computerWinsText = document.querySelector("#computerWins");
+const tiesText = document.querySelector("#ties");
+
+const divResetGame = document.querySelector("#resetGame");
+const victoryStatusMessage = document.querySelector("#victoryStatus");
+const btnResetGame = document.querySelector("#resetGameBtn");
+
+/* F1 - End game once one player reaches 5 victories */
+
+function endGame() {
+    if(wins === 5 | losses === 5) {
+
+        btnRock.setAttribute('disabled', '');
+        btnPaper.setAttribute('disabled', '');
+        btnScissors.setAttribute('disabled', '');
+
+        if(wins === 5) {
+            victoryStatusMessage.textContent = 'Congratulations! You have vanquished the computer!'
+        } else if(losses === 5) {
+            victoryStatusMessage.textContent = 'Alas! You lost this fight. Maybe next time...'
+        }
+
+        divResetGame.removeAttribute('style', 'display: none');
+    }
+}
+
+/* F2 - Reset the game when "play again" button is pressed */
+
+function resetGame() {
+    btnRock.removeAttribute('disabled');
+        btnPaper.removeAttribute('disabled');
+        btnScissors.removeAttribute('disabled');
+
+    playerChoiceImg.removeAttribute('src');
+    playerChoiceImg.removeAttribute('alt');
+    playerChoiceName.textContent = '';
+
+    computerChoiceImg.removeAttribute('src');
+    computerChoiceImg.removeAttribute('alt');
+    computerChoiceName.textContent = '';
+
+    roundResult.textContent = '';
+
+    wins = 0;
+    losses = 0;
+    ties = 0;
+    
+    playerWinsText.textContent = wins;
+    computerWinsText.textContent = losses;
+    tiesText.textContent = ties;
+
+    divResetGame.setAttribute('style', 'display: none');
+}
+
+btnResetGame.addEventListener('click', resetGame);
+
+/* F3 - Generate random choice: rock, paper or scissors */
 
 function getComputerChoice() {
     const randomNumber = Math.floor(Math.random() * (4-1) + 1);
@@ -24,26 +84,7 @@ function getComputerChoice() {
     }
 }
 
-/* F2 - Get player's choice: rock, paper or scissors. Disallow other inputs
-
-function getPlayerChoice() {
-    let choice = prompt('Pick: rock, paper or scissors', '').toLowerCase();
-    
-    if(choice === 'rock' | choice === 'paper' | choice === 'scissors') {
-        return choice;
-    } else {
-        while(choice !== 'rock' && choice !== 'paper' && choice !== 'scissors'){
-            choice = prompt('Invalid choice. Choose rock, paper or scissors.', '').toLowerCase();
-        }
-        return choice;
-    }
-} */
-
-/* F3 - Play 1 round of rock, paper, scissors. Return victory, loss or tie
-    Requires F1 and F2 */
-let wins = 0;
-let losses = 0;
-let ties = 0;
+/* F4 - Play 1 round when "rock", "paper", or "scissors" button is pressed */
 
 function playRound(choice) {
     const computerSelection = getComputerChoice();
@@ -81,72 +122,24 @@ function playRound(choice) {
         const winMessage = `You win this round! ${playerSelection} beats ${computerSelection}.`;
         wins += 1;
         roundResult.textContent = winMessage;
-        //return winMessage;
+        playerWinsText.textContent = wins;
     } else if((playerSelection === 'rock' && computerSelection === 'paper') | (playerSelection === 'scissors' && computerSelection === 'rock') | (playerSelection === 'paper' && computerSelection === 'scissors')) {
         const loseMessage = `You lose this round! ${computerSelection} beats ${playerSelection}.`;
         losses += 1;
         roundResult.textContent = loseMessage;
-        //return loseMessage;
+        computerWinsText.textContent = losses;
     } else if (playerSelection === computerSelection) {
         const tieMessage = `It's a tie this round! ${playerSelection} and ${computerSelection}.`;
         ties += 1;
         roundResult.textContent = tieMessage;
-        //return tieMessage;
+        tiesText.textContent = ties;
     } else {
         roundResult.textContent = 'Oops! Something went wrong. Sorry.';
-        //return 'Oops! Something went wrong. Sorry.';
     }
+
+    endGame();
 }
 
 btnRock.addEventListener('click', () => playRound("rock"));
 btnPaper.addEventListener('click', () => playRound("paper"));
 btnScissors.addEventListener('click', () => playRound("scissors"));
-
-/* F4 - Reset and restart the game
-    Requires F5 */
-function resetGame() {
-    alert('Play again?');
-    wins = 0;
-    losses = 0;
-    ties = 0;
-
-    game();
-}
-
-/* F5 - Play a 5 round game. Return final victory, loss or tie. Restart game
-    Requires F3 and F4 */
-/* function game() {
-    let result;
-  
-    for(let i = 0; i < 5; i++) {
-
-        console.log(playRound());
-
-    }
-
-    if(wins > losses) {
-        console.log(result = `You Won! Yay!
-        Games won: ${wins}
-        Games lost: ${losses}
-        Games tied: ${ties}`);
-        
-    } else if(wins < losses) {
-        console.log(result = `You Lost! Maybe next time...
-        Games won: ${wins}
-        Games lost: ${losses}
-        Games tied: ${ties}`);
-        
-    } else if(wins === losses) {
-        console.log(result = `You tried, you tied!
-        Games won: ${wins}
-        Games lost: ${losses}
-        Games tied: ${ties}`);
-        
-    } else {
-        console.log('Oops! Something went wrong. Sorry.');
-    }
-
-    resetGame();
-} 
-
-console.log(game()); */
